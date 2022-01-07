c128933872426c32dfa98d0863195e46498afd69
Tiger0131 <97296034+Tiger0131@users.noreply.github.com>
2022-01-07 23:02:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = 14435801
-    API_HASH = 345a27c98808d5a46382a48e4b22e235
-    BOT_TOKEN = AAFgWxy04xGcxkhvSnkI7kZTM1DyB3K-68g
+    API_ID = 
+    API_HASH = 
+    BOT_TOKEN = 
